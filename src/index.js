1881d0612e7f0432653486dc4b3de684e1f134f7
Dalanaran <dalagar00@gmail.com>
2022-12-22 23:26:30 +0200

    Slider

--- src/index.js
+++ src/index.js
@@ -17,8 +17,6 @@
 
 // -----Modal Window-----
 
-// -----Slider-----
-
 setInterval(() => {
 	let $curr = document.querySelector('#slider1 input[type=radio]:checked'),
   		$next = $curr.nextSibling;
@@ -29,4 +27,3 @@ setInterval(() => {
   $next.setAttribute('checked', true);
 }, 3000);
 
-// -----Slider-----
