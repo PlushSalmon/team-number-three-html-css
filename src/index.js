926daf02529f22a8d5706e64c059129bd8a430d0
dyonns <117724779+dyonns@users.noreply.github.com>
2022-12-22 22:02:50 +0200

    edits 3

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@
 })();
 
 // -----Modal Window-----
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
