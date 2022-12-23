869326c6607086884de82db226a2deaa5fb9a804
Dalanaran <dalagar00@gmail.com>
2022-12-23 12:58:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -82,3 +82,20 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+// ------Pop Up 5-----
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open6]"),
+    closeModalBtn: document.querySelector("[data-modal-close6]"),
+    modal: document.querySelector("[data-modal6]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
